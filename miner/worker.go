fa1302abcce130c70a9b8152bf413bd75a6541a1
j75689 <j75689@gmail.com>
2022-05-07 01:15:26 +0800

    fix worker

--- miner/worker.go
+++ miner/worker.go
@@ -859,7 +859,7 @@ func (w *worker) commitTransactions(env *environment, txs *types.TransactionsByP
 	gasLimit := env.header.GasLimit
 	if env.gasPool == nil {
 		env.gasPool = new(core.GasPool).AddGas(gasLimit)
-		if w.chain.Config().IsEuler(w.current.header.Number) {
+		if w.chain.Config().IsEuler(env.header.Number) {
 			env.gasPool.SubGas(params.SystemTxsGas * 3)
 		} else {
 			env.gasPool.SubGas(params.SystemTxsGas)
